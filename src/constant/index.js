2249de21cd28906abc55a51dc0c5fc2225e65a3e
KNU-K <knukang334@gmail.com>
2024-11-13 13:38:35 +0900

    feat: add to constant definition

---
+++ src/constant/index.js
@@ -0,0 +1,9 @@
+//program information
+exports.PROGRAM_NAME = "changelog-gen";
+exports.PROGRAM_DESCRIPTION = "Project to quickly create changelog.md automatically based on Angular commit convention";
+exports.PROGRAM_VERSION = "1.0.0";
+
+//git information
+exports.LOG_FILE = "changelog.md";
+exports.LAST_COMMIT_FILE = "logged_commit.txt";
+exports.COMMIT_TYPES = ["build", "ci", "docs", "feat", "fix", "perf", "refactor", "style", "test"];
